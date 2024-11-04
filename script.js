c1f4bb61ab0bbd29bfb4f69058a7c5fb6f9e1251
jackdeleo <152327815+jackdeleo@users.noreply.github.com>
2024-11-03 21:35:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+//Group member contribution
+//script.js -> Jack DeLeo
 
+$(document).ready(function() {
+    $('#productForm').on('submit', function(event) {
+        event.preventDefault();
+        const productData = {
+            id: $('#productId').val(),
+            description: $('#productDescription').val(),
+            category: $('#productCategory').val(),
+            unitOfMeasure: $('#productUOM').val(),
+            price: $('#productPrice').val(),
+            weight: $('#productWeight').val() || null
+        };
+        $('#jsonOutput').text(JSON.stringify(productData, null, 2));
+    });
+});
